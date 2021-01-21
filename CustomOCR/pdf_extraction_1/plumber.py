7dc042dd8a0120fbe20ce78c3c5419b0887ca771
jan.babel <jan.babel@emeldi.com>
2021-01-21 20:27:08 +0100

    rozsirenie citania blocka + pridanie kostry pre dalsie rozpoznavanie, v pripade, ze sa nejadna o blocek

--- CustomOCR/pdf_extraction_1/plumber.py
+++ CustomOCR/pdf_extraction_1/plumber.py
@@ -1,35 +1,42 @@
 import pdfplumber
 import re
 import unidecode
-import PyMuPDF
-
+from pyzbar.pyzbar import decode
+from pdf2image import convert_from_path
+import cv2
 from CustomOCR.pdf_extraction_1.utils import *
+import requests
+from os.path import splitext
 
 path = '..\\..\\Datasets\\faktury\\faktury_csob\\'
+# filename = 'peugeot.pdf'
+# filename = 'melisko.pdf'
 filename = 'autonova.pdf'
 # filename = 'impa.pdf'
+# filename = 'blocek1.jpg'
+# filename = 'lidl.pdf'
+full_path = path + filename
 
-percentage = get_text_percentage(path + filename)
-print(percentage)
+# pokladnicny blok
+extracted_values = extract_qr_code(full_path)
 
-extracted_values ={}
+if not extracted_values:
+    # faktura
+    with pdfplumber.open(full_path) as pdf:
+        first_page = pdf.pages[0]
+        extracted_text = first_page.extract_text()
+        if extracted_text:
+            print('Extrahujem text z PDF')
+            unaccented_upper_text = unidecode.unidecode(extracted_text.upper())
+            extracted_values = extract_pdf_text(unaccented_upper_text)
+        else:
+            print('pouzivam OCR')
+            pdf = convert_from_path(full_path)
+            extracted_values = extract_dynamic_fields(pdf, ['úhrade','IBAN'])  # spravit dicsitoonary.. uhrada:decimal  IBAN:iban
 
-with pdfplumber.open(path+filename) as pdf:
-    first_page = pdf.pages[0]
-    extracted_text = first_page.extract_text()
-    if extracted_text:
-        print('Extrahujem text z PDF')
-        unaccented_upper_text = unidecode.unidecode(extracted_text.upper())
-        extracted_values = extract_pdf_text(unaccented_upper_text)
-    # print(unaccented_upper_text)
-    else:
-        print('pouzivam OCR')
+# rukou pisana faktura
+elif not extracted_values:
+    print('rukou pisana faktura')
 
 
-target_values = load_target_values(filename)
-
-print('target_values =>', target_values)
-print('extracted_values =>',extracted_values)
-
-accuracy = calculate_accuracy(target_values, extracted_values)
-print('presnost = ', accuracy, ' %')
+calculate_accuracy(filename, extracted_values)
--- CustomOCR/pdf_extraction_1/utils.py
+++ CustomOCR/pdf_extraction_1/utils.py
@@ -1,11 +1,22 @@
 import re
 import json
+import re
+import unidecode
+from pyzbar.pyzbar import decode
+from pdf2image import convert_from_path
+import cv2
+from CustomOCR.pdf_extraction_1.utils import *
+import requests
+from os.path import splitext
+import pytesseract
+import numpy as np
 
 amount_whitelist_characters = re.compile('[^0-9,.]')
 reg_total_amount = 'UHRADE(.*)(e|eur|EUR|€|euro)(?:\s|$)'
 reg_ecv = '(B(A|B|C|J|L|N|R|S|Y|T)|CA|D(K|S|T)|G(A|L)|H(C|E)|IL|K(A|I|E|K|M|N|S)|L(E|C|M|V)|M(A|I|L|T|Y)|N(I|O|M|R|Z)|P(B|D|E|O|K|N|P|T|U|V)|R(A|K|S|V)|S(A|B|C|E|I|K|L|O|N|P|V)|T(A|C|N|O|R|S|T|V)|V(K|T)|Z(A|C|H|I|M|V))([ |-]{0,1})([0-9]{3})([A-Z]{2})'
 reg_iban = 'SK\d{2}\s?\d{4}\s?\d{4}\s?\d{4}\s?\d{4}\s?\d{4}|SK\d{22}'
 reg_vin = '^([A-HJ-NPR-Z\d]{3})([A-HJ-NPR-Z\d]{5})([\dX])(([A-HJ-NPR-Z\d])([A-HJ-NPR-Z\d])([A-HJ-NPR-Z\d]{6}))$'
+
 
 def extract_pdf_text(unaccented_upper_text):
     extracted_values = {}
@@ -32,7 +43,7 @@ def extract_pdf_text(unaccented_upper_text):
         # print(IBAN.group())
         extracted_values.update({'iban': IBAN.group()})
 
-    #VIN
+    # VIN
     vin = re.search(reg_vin, unaccented_upper_text)
     if vin:
         # print(vin.group())
@@ -40,8 +51,10 @@ def extract_pdf_text(unaccented_upper_text):
 
     return extracted_values
 
+
 def re_replace(string):
     return re.sub(amount_whitelist_characters, '', string)
+
 
 def load_target_values(filename):
     with open('validation\\validation.json', encoding='utf-8') as f:
@@ -51,37 +64,125 @@ def load_target_values(filename):
                 return file['values']
 
 
-def calculate_accuracy(target, actual):
+def calculate_accuracy(filename, extracted_values):
+    target_values = load_target_values(filename)
+
+    print('target_values =>', target_values)
+    print('extracted_values =>', extracted_values)
     count = 0
-    for key in target.keys():
-        if target.get(key) == actual.get(key):
-            count +=1
-    return count/len(target.keys())*100
+    for key in target_values.keys():
+        if target_values.get(key) == extracted_values.get(key):
+            count += 1
+    accuracy = count / len(target_values.keys()) * 100
+    print('presnost = ', accuracy, ' %')
 
 
-import fitz
+def extract_qr_code(full_path):
+    extracted_values = {}
+    extension = splitext(full_path)[1]
+    qr = None
+    if extension in ('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif'):
+        img = cv2.imread(full_path)
+        qr = decode(img)
+    elif extension == '.pdf':
+        img = convert_from_path(full_path)
+        for pageNumber, page in enumerate(img):
+            qr = decode(page)
+    else:
+        print('Zadany format dokumentu nie je podporovany')
+
+    if qr:  # ak naslo zober prvy
+        qrcode = qr[0].data.decode('utf-8')
+        ekasa_response = requests.post('https://ekasa.financnasprava.sk/mdu/api/v1/opd/receipt/find',
+                                       json={"receiptId": qrcode})
+        if ekasa_response:
+            extracted_values.update({'suma': str(ekasa_response.json()['receipt']['totalPrice']).replace('.', ',')})
+
+    return extracted_values
+
+
+def extract_dynamic_fields(page, phrases):
+    dynamic_fields = {}
+    image = np.array(page[0])
+    height, width = image.shape[:2]
+    # converting image into gray scale image
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    threshold_img = cv2.threshold(gray_image, 100, 255, cv2.THRESH_BINARY)[1]
+    threshold_img1 = cv2.threshold(gray_image, 100, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    dst = cv2.resize(threshold_img, (int(width / 2), int(height / 2)))
+    # cv2.imshow('threshold_img ',dst)
+    # cv2.waitKey(0 )
+    # extract fields
+    # gfg = pytesseract.image_to_data(threshold_img, lang='SLK')
+    extracted_dynamic_fields = pytesseract.image_to_data(threshold_img, lang='SLK', output_type='data.frame')
+    extracted_dynamic_fields_temp = pytesseract.image_to_data(threshold_img, lang='SLK')
+    extracted_dynamic_fields1 = pytesseract.image_to_data(threshold_img1, lang='SLK', output_type='data.frame')
+    # extracted_dynamic_fields1_temp = pytesseract.image_to_data(threshold_img1, lang='SLK')
+    print(extracted_dynamic_fields_temp)
+    # print(extracted_dynamic_fields1_temp)
+    # add as key:value pair
+    for i, phrase in enumerate(phrases):
+        target_word = check_and_extract(phrase, extracted_dynamic_fields)
+        if not target_word:
+            target_word = check_and_extract(phrase, extracted_dynamic_fields1)
+
+        if target_word is not None:
+            # dynamic_fields.update({phrase['name']: target_word})
+            dynamic_fields.update({phrase: target_word})
+
+    # return JSON like object with phrase name and extracted values (amounts in EUR, etc..)  -> sum_total:20,26
+    return dynamic_fields
+
+def check_and_extract(phrase, extracted_dynamic_fields):
+    # extract the correct row and all words in a row by searching for the template phrase
+    correct_row = extract_correct_row(extracted_dynamic_fields, phrase)
+    # find final value in a row through regexp
+    final_value = find_final_value(str(correct_row),phrase)
+
+    return final_value
 
 
 
-def get_text_percentage(file_name: str) -> float:
-    """
-    Calculate the percentage of document that is covered by (searchable) text.
+# find the value based on defined type
+def find_final_value(row, template_type):
+    if template_type =='decimal':
+        return re.search('\d+[.,]\d+', str(row)).group()
+    # if template_type['type'] in 'decimal':
+    #     return re.findall('\d+[.,]\d+', str(row))
+    # elif template_type['type'] in 'integer':
+    #     return re.findall('\d+', str(row))
+    elif template_type =='IBAN':
+        row1 = re.sub(r'[^a-zA-Z0-9]', '', row)
+        return re.search(reg_iban, str(row1)).group()
 
-    If the returned percentage of text is very low, the document is
-    most likely a scanned PDF
-    """
-    total_page_area = 0.0
-    total_text_area = 0.0
 
-    doc = fitz.open(file_name)
-
-    for page_num, page in enumerate(doc):
-        total_page_area = total_page_area + abs(page.rect)
-        text_area = 0.0
-        for b in page.getTextBlocks():
-            r = fitz.Rect(b[:4])  # rectangle where block text appears
-            text_area = text_area + abs(r)
-        total_text_area = total_text_area + text_area
-    doc.close()
-    return total_text_area / total_page_area
-print("not fully scanned PDF - text is present")
+def extract_correct_row(extracted_dynamic_fields, phrase):
+    # split tepmlate phrase into words
+    # text_split = phrase['text'].split()
+    text_split = [phrase,phrase+':']
+    # take first word from phrase and find its coordinates.. the word can occurs multiple time on page
+    top_values = extracted_dynamic_fields[extracted_dynamic_fields['text'] == text_split[0]]['top'].values
+    if not top_values:
+        top_values = extracted_dynamic_fields[extracted_dynamic_fields['text'] == text_split[1]]['top'].values
+    rows = {}
+    # loop through all TOP coordinates of the 1. word
+    for top in top_values:
+        # create the range of TOP pixels for 1. word, because words in row can be upper or lower case and therefore the distance from the TOP is changing slightly <top-1, top +5>  because usualy the 1. word is Upper case
+        pixels_from_top_range = np.arange(top - 7, top + 7, ).tolist()
+        # all words that were found +- around same distance from Top of the page
+        row_text = extracted_dynamic_fields[extracted_dynamic_fields['top'].isin(pixels_from_top_range)]['text'].values
+        # clean the row by replacing non values and spaces
+        clear_row_text = str(row_text).replace('nan', '').replace(' ', '')
+        # if there is a word, then put it into the dictionary
+        if len(row_text) != 0:
+            rows.update({top: clear_row_text})
+    # while 1 word can be found in multiple row on a page, we need to find the correct row by comparing the occurence of the phrase from template with the extracted row. If all words from template exists in the extracted row, then thats the correct row.
+    for row in rows:
+        # split each row into arrays so we can easily compares the subarrays
+        correct_row = rows[row].replace('\'', ' ').split()
+        # checks if ALL words from template exists in row. If some word is missing then the not the correct row or the extraction failed.
+        # is_all_words_found = all(item in correct_row for item in phrase['text'].split())
+        # if is_all_words_found:
+        if True:
+            # if all words matched then its correct row
+            return correct_row
