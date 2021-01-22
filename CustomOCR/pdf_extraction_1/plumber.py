b0373cbfd6e52706504608d48e26f2a0dd559dd7
jan.babel <jan.babel@emeldi.com>
2021-01-22 19:52:36 +0100

    dalsie vychytavky pre OCR

--- CustomOCR/pdf_extraction_1/plumber.py
+++ CustomOCR/pdf_extraction_1/plumber.py
@@ -9,13 +9,21 @@ import requests
 from os.path import splitext
 
 path = '..\\..\\Datasets\\faktury\\faktury_csob\\'
+# filename = 'anders.jpg'   # square
+# filename = 'autofun.pdf'
+filename = 'autosklo.jpg'
 # filename = 'peugeot.pdf'
+# filename = 'suzuki1.pdf'
+# filename = 'suzuki2.pdf'
+# filename = 'suzuki3.pdf'
 # filename = 'melisko.pdf'
-filename = 'autonova.pdf'
-# filename = 'impa.pdf'
+# filename = 'autonova.pdf'
+# filename = 'impa.pdf'      # 75 %
 # filename = 'blocek1.jpg'
 # filename = 'lidl.pdf'
 full_path = path + filename
+
+phrases_to_extract = {'suma': 'ÚHRADE', 'iban': 'IBAN'}
 
 # pokladnicny blok
 extracted_values = extract_qr_code(full_path)
@@ -25,14 +33,17 @@ if not extracted_values:
     with pdfplumber.open(full_path) as pdf:
         first_page = pdf.pages[0]
         extracted_text = first_page.extract_text()
-        if extracted_text:
+        if bool(extracted_text) and any(char.isdigit() for char in extracted_text):
             print('Extrahujem text z PDF')
             unaccented_upper_text = unidecode.unidecode(extracted_text.upper())
+            print(unaccented_upper_text)
             extracted_values = extract_pdf_text(unaccented_upper_text)
         else:
             print('pouzivam OCR')
             pdf = convert_from_path(full_path)
-            extracted_values = extract_dynamic_fields(pdf, ['úhrade','IBAN'])  # spravit dicsitoonary.. uhrada:decimal  IBAN:iban
+
+            phrases = phrases_to_extract
+            extracted_values = extract_dynamic_fields(pdf, phrases)  # spravit dicsitoonary.. uhrada:decimal  IBAN:iban
 
 # rukou pisana faktura
 elif not extracted_values:
--- CustomOCR/pdf_extraction_1/utils.py
+++ CustomOCR/pdf_extraction_1/utils.py
@@ -13,9 +13,10 @@ import numpy as np
 
 amount_whitelist_characters = re.compile('[^0-9,.]')
 reg_total_amount = 'UHRADE(.*)(e|eur|EUR|€|euro)(?:\s|$)'
+reg_total_amount1 = 'UHRAD(.*)(e|eur|EUR|€|euro)(?:\s|$)\d+\s*\d+\s*\d+[.,]?\d+'
 reg_ecv = '(B(A|B|C|J|L|N|R|S|Y|T)|CA|D(K|S|T)|G(A|L)|H(C|E)|IL|K(A|I|E|K|M|N|S)|L(E|C|M|V)|M(A|I|L|T|Y)|N(I|O|M|R|Z)|P(B|D|E|O|K|N|P|T|U|V)|R(A|K|S|V)|S(A|B|C|E|I|K|L|O|N|P|V)|T(A|C|N|O|R|S|T|V)|V(K|T)|Z(A|C|H|I|M|V))([ |-]{0,1})([0-9]{3})([A-Z]{2})'
 reg_iban = 'SK\d{2}\s?\d{4}\s?\d{4}\s?\d{4}\s?\d{4}\s?\d{4}|SK\d{22}'
-reg_vin = '^([A-HJ-NPR-Z\d]{3})([A-HJ-NPR-Z\d]{5})([\dX])(([A-HJ-NPR-Z\d])([A-HJ-NPR-Z\d])([A-HJ-NPR-Z\d]{6}))$'
+reg_vin = '(([a-h,A-H,j-n,J-N,p-z,P-Z,0-9]{9})([a-h,A-H,j-n,J-N,p,P,r-t,R-T,v-z,V-Z,0-9])([a-h,A-H,j-n,J-N,p-z,P-Z,0-9])\s*(\d{6}))'
 
 
 def extract_pdf_text(unaccented_upper_text):
@@ -23,6 +24,8 @@ def extract_pdf_text(unaccented_upper_text):
 
     # SUMA
     amount_str = re.search(reg_total_amount, unaccented_upper_text)
+    if not amount_str:
+        amount_str = re.search(reg_total_amount1, unaccented_upper_text)
     if amount_str:
         m = re.search('\d', amount_str.group())
         if m:
@@ -44,10 +47,16 @@ def extract_pdf_text(unaccented_upper_text):
         extracted_values.update({'iban': IBAN.group()})
 
     # VIN
-    vin = re.search(reg_vin, unaccented_upper_text)
+    vin = re.findall(reg_vin, unaccented_upper_text)
     if vin:
+        for i, v in enumerate(vin):
+            print(vin[i][0])
+            if (re.search('SK\d{2}', vin[i][0])):
+                continue
+            else:
+                extracted_values.update({'vin': vin[i][0].replace(' ','')})
         # print(vin.group())
-        extracted_values.update({'vin': vin.group()})
+
 
     return extracted_values
 
@@ -91,7 +100,7 @@ def extract_qr_code(full_path):
     else:
         print('Zadany format dokumentu nie je podporovany')
 
-    if qr:  # ak naslo zober prvy
+    if qr and qr[0][1]=='QRCODE':  # ak naslo zober prvy
         qrcode = qr[0].data.decode('utf-8')
         ekasa_response = requests.post('https://ekasa.financnasprava.sk/mdu/api/v1/opd/receipt/find',
                                        json={"receiptId": qrcode})
@@ -103,7 +112,7 @@ def extract_qr_code(full_path):
 
 def extract_dynamic_fields(page, phrases):
     dynamic_fields = {}
-    image = np.array(page[0])
+    image = np.array(page[0])  ### !!! TODO tu treba loopovat bacha, iba prva strana
     height, width = image.shape[:2]
     # converting image into gray scale image
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -117,41 +126,39 @@ def extract_dynamic_fields(page, phrases):
     extracted_dynamic_fields = pytesseract.image_to_data(threshold_img, lang='SLK', output_type='data.frame')
     extracted_dynamic_fields_temp = pytesseract.image_to_data(threshold_img, lang='SLK')
     extracted_dynamic_fields1 = pytesseract.image_to_data(threshold_img1, lang='SLK', output_type='data.frame')
-    # extracted_dynamic_fields1_temp = pytesseract.image_to_data(threshold_img1, lang='SLK')
+    extracted_dynamic_fields1_temp = pytesseract.image_to_data(threshold_img1, lang='SLK')
     print(extracted_dynamic_fields_temp)
     # print(extracted_dynamic_fields1_temp)
     # add as key:value pair
-    for i, phrase in enumerate(phrases):
-        target_word = check_and_extract(phrase, extracted_dynamic_fields)
+    for i, (key,value) in enumerate(phrases.items()):
+        target_word = check_and_extract(key,value, extracted_dynamic_fields)
         if not target_word:
-            target_word = check_and_extract(phrase, extracted_dynamic_fields1)
+            target_word = check_and_extract(key,value, extracted_dynamic_fields1)
 
         if target_word is not None:
             # dynamic_fields.update({phrase['name']: target_word})
-            dynamic_fields.update({phrase: target_word})
+            dynamic_fields.update({key: target_word})
 
     # return JSON like object with phrase name and extracted values (amounts in EUR, etc..)  -> sum_total:20,26
     return dynamic_fields
 
-def check_and_extract(phrase, extracted_dynamic_fields):
+def check_and_extract(key,phrase, extracted_dynamic_fields):
     # extract the correct row and all words in a row by searching for the template phrase
     correct_row = extract_correct_row(extracted_dynamic_fields, phrase)
     # find final value in a row through regexp
-    final_value = find_final_value(str(correct_row),phrase)
-
-    return final_value
-
+    if correct_row:
+        return find_final_value(str(correct_row),key)
 
 
 # find the value based on defined type
 def find_final_value(row, template_type):
-    if template_type =='decimal':
-        return re.search('\d+[.,]\d+', str(row)).group()
+    if template_type =='suma':
+        return re.search('\d+\s*\d+\s*\d+[.,]?\d+', str(row)).group().replace(' ','')
     # if template_type['type'] in 'decimal':
     #     return re.findall('\d+[.,]\d+', str(row))
     # elif template_type['type'] in 'integer':
     #     return re.findall('\d+', str(row))
-    elif template_type =='IBAN':
+    elif template_type =='iban':
         row1 = re.sub(r'[^a-zA-Z0-9]', '', row)
         return re.search(reg_iban, str(row1)).group()
 
@@ -161,16 +168,21 @@ def extract_correct_row(extracted_dynamic_fields, phrase):
     # text_split = phrase['text'].split()
     text_split = [phrase,phrase+':']
     # take first word from phrase and find its coordinates.. the word can occurs multiple time on page
+    extracted_dynamic_fields['text'] = extracted_dynamic_fields['text'].str.upper()
     top_values = extracted_dynamic_fields[extracted_dynamic_fields['text'] == text_split[0]]['top'].values
+    # left_values = extracted_dynamic_fields[extracted_dynamic_fields['text'] == text_split[0]]['left'].values
+
     if not top_values:
         top_values = extracted_dynamic_fields[extracted_dynamic_fields['text'] == text_split[1]]['top'].values
+        # left_values = extracted_dynamic_fields[extracted_dynamic_fields['text'] == text_split[1]]['left'].values
     rows = {}
     # loop through all TOP coordinates of the 1. word
     for top in top_values:
         # create the range of TOP pixels for 1. word, because words in row can be upper or lower case and therefore the distance from the TOP is changing slightly <top-1, top +5>  because usualy the 1. word is Upper case
-        pixels_from_top_range = np.arange(top - 7, top + 7, ).tolist()
+        pixels_from_top_range = np.arange(top - 8, top + 100, ).tolist()
         # all words that were found +- around same distance from Top of the page
         row_text = extracted_dynamic_fields[extracted_dynamic_fields['top'].isin(pixels_from_top_range)]['text'].values
+        # after_text = extracted_dynamic_fields[extracted_dynamic_fields['left'].isin( np.arange(left_values,left_values+ 400, ).tolist())]['text'].values
         # clean the row by replacing non values and spaces
         clear_row_text = str(row_text).replace('nan', '').replace(' ', '')
         # if there is a word, then put it into the dictionary
@@ -179,7 +191,8 @@ def extract_correct_row(extracted_dynamic_fields, phrase):
     # while 1 word can be found in multiple row on a page, we need to find the correct row by comparing the occurence of the phrase from template with the extracted row. If all words from template exists in the extracted row, then thats the correct row.
     for row in rows:
         # split each row into arrays so we can easily compares the subarrays
-        correct_row = rows[row].replace('\'', ' ').split()
+        # correct_row = rows[row].replace('\'', ' ').split()
+        correct_row = rows[row].replace('\'', ' ')
         # checks if ALL words from template exists in row. If some word is missing then the not the correct row or the extraction failed.
         # is_all_words_found = all(item in correct_row for item in phrase['text'].split())
         # if is_all_words_found:
