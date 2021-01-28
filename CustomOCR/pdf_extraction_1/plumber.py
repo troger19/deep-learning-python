9600b870cc610d04c13d99ba91b22939ed3e2b71
jan.babel <jan.babel@emeldi.com>
2021-01-28 11:53:25 +0100

    reading target values from excel sheet, looping through multiple invoice pages,

--- CustomOCR/pdf_extraction_1/plumber.py
+++ CustomOCR/pdf_extraction_1/plumber.py
@@ -29,36 +29,39 @@ path = '..\\..\\Datasets\\faktury\\pokus\\'
 # phrases_to_extract = {'suma': 'ÚHRADE', 'iban': 'IBAN'}
 invoices_list = os.listdir(path)
 phrases_to_extract = {'suma': 'SUMA ÚHRADE', 'iban': 'IBAN','vin': 'VIN','ecv': 'ECV'}
-extraction_method='QR'
+
 
 def extract_values_from_file(full_path):
+    img_pdf = None
     start_time = time.time()
-
+    extraction_method =None
 # pokladnicny blok
-    extracted_values = extract_qr_code(full_path)
+    extracted_values,extraction_method = extract_qr_code(full_path,extraction_method)
 
     extension = splitext(full_path)[1]
     if not extracted_values:
         if extension =='.pdf':
         # faktura
             with pdfplumber.open(full_path) as pdf:
-                first_page = pdf.pages[0]
-                extracted_text = first_page.extract_text()
-                if bool(extracted_text) and any(char.isdigit() for char in extracted_text):
-                    print('Extrahujem text z PDF')
-                    extraction_method = 'PDF TEXT'
-                    unaccented_upper_text = unidecode.unidecode(extracted_text.upper())
-                    # print(unaccented_upper_text)
-                    extracted_values = extract_pdf_text(unaccented_upper_text)
-                else:
-                    print('pouzivam OCR')
-                    extraction_method = 'OCR'
-                    pdf = convert_from_path(full_path)
-                    extracted_values = extract_dynamic_fields(np.array(pdf[len(pdf)-1]), phrases_to_extract)  # spravit dicsitoonary.. uhrada:decimal  IBAN:iban
+                for i,page in enumerate(pdf.pages):
+                    first_page =page
+                    extracted_text = first_page.extract_text()
+                    if bool(extracted_text) and any(char.isdigit() for char in extracted_text):
+                        print('Extrahujem text z PDF')
+                        extraction_method = 'PDF TEXT'
+                        unaccented_upper_text = unidecode.unidecode(extracted_text.upper())
+                        # print(unaccented_upper_text)
+                        extracted_values = extract_pdf_text(unaccented_upper_text,extracted_values)
+                    else:
+                        print('pouzivam OCR')
+                        extraction_method = 'OCR'
+                        if img_pdf is None:
+                            img_pdf = convert_from_path(full_path)
+                        extracted_values = extract_dynamic_fields(np.array(img_pdf[i]), phrases_to_extract,extracted_values)
         elif extension in ('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif'):
             extraction_method = 'OCR'
-            img = cv2.imread(full_path)
-            extracted_values = extract_dynamic_fields(img, phrases_to_extract)
+            img = cv2.imdecode(np.fromfile(full_path, dtype=np.uint8), -1)
+            extracted_values = extract_dynamic_fields(img, phrases_to_extract,extracted_values)
         else:
             print('Neznamy format vstupuneho suboru')
     # rukou pisana faktura
@@ -68,10 +71,14 @@ def extract_values_from_file(full_path):
     elapsed_time = time.time() - start_time
     elapsed_time = time.strftime("%M:%S", time.gmtime(elapsed_time))
     print(elapsed_time)
-    calculate_accuracy(os.path.basename(full_path),extraction_method, extracted_values,elapsed_time)
 
+    target_values = all_target_values.get(os.path.basename(full_path),{})
+    calculate_accuracy(os.path.basename(full_path),target_values,extraction_method, extracted_values,elapsed_time)
+
+all_target_values = load_target_values_excel1()
 for i,y in enumerate(invoices_list):
     print(y)
     extract_values_from_file(path+y)
+
 
 
--- CustomOCR/pdf_extraction_1/utils.py
+++ CustomOCR/pdf_extraction_1/utils.py
@@ -11,29 +11,30 @@ from os.path import splitext
 import pytesseract
 import numpy as np
 import csv
+import xlrd
+import openpyxl
 
 amount_whitelist_characters = re.compile('[^0-9,.]')
-reg_total_amount = 'UHRADE(.*)(e|eur|EUR|€|euro)(?:\s|$)'
-reg_total_amount1 = 'UHRAD(.*)(e|eur|EUR|€|euro)(?:\s|$)\d+\s*\d+\s*\d+[.,]?\d+'
+reg_total_amount2 = ['UHRADE(.*)(e|eur|EUR|€|euro)(?:\s|$)', 'SUMA(.*)(e|eur|EUR|€|euro)(?:\s|$)']
+
 reg_ecv = '(B(A|B|C|J|L|N|R|S|Y|T)|CA|D(K|S|T)|G(A|L)|H(C|E)|IL|K(A|I|E|K|M|N|S)|L(E|C|M|V)|M(A|I|L|T|Y)|N(I|O|M|R|Z)|P(B|D|E|O|K|N|P|T|U|V)|R(A|K|S|V)|S(A|B|C|E|I|K|L|O|N|P|V)|T(A|C|N|O|R|S|T|V)|V(K|T)|Z(A|C|H|I|M|V))([ |-]{0,1})([0-9]{3})([A-Z]{2})'
 reg_iban = '[5S]K\d{2}\s?\d{4}\s?\d{4}\s?\d{4}\s?\d{4}\s?\d{4}|SK\d{22}'
 reg_vin = '(([a-h,A-H,j-n,J-N,p-z,P-Z,0-9]{9})([a-h,A-H,j-n,J-N,p,P,r-t,R-T,v-z,V-Z,0-9])([a-h,A-H,j-n,J-N,p-z,P-Z,0-9])\s*(\d{6}))'
-reg_total_amount_number = ['\d+\s*\d+\s*\d+[.,]?\d+','\d+[.,]?\d+']
+reg_total_amount_number = ['\d+\s*\d+\s*\d+[.,]?\d+', '\d+[.,]?\d+']
+reg_ico = ['ICO\s*?.?\s*?(\d{8})','ICO\s*?.?\s*?(\d{2} \d{3} \d{3})']
 
-def extract_pdf_text(unaccented_upper_text):
-    extracted_values = {}
 
+def extract_pdf_text(unaccented_upper_text, extracted_values):
+    extracted_values = extracted_values
     # SUMA
-    amount_str = re.search(reg_total_amount, unaccented_upper_text)
-    if not amount_str:
-        amount_str = re.search(reg_total_amount1, unaccented_upper_text)
+    amount_str = try_multiple_regex(unaccented_upper_text, reg_total_amount2)
     if amount_str:
-        m = re.search('\d', amount_str.group())
+        m = re.search('\d', amount_str)
         if m:
-            amount_str = amount_str.group()[m.start():]
+            amount_str = amount_str[m.start():]
             amount = re_replace(amount_str)
             # print(amount)
-            extracted_values.update({'suma': amount})
+            extracted_values.update({'cena_s_dph': amount})
 
     # ECV
     license_plate_number = re.search(reg_ecv, unaccented_upper_text)
@@ -55,9 +56,13 @@ def extract_pdf_text(unaccented_upper_text):
             if (re.search('SK\d{2}', vin[i][0])):
                 continue
             else:
-                extracted_values.update({'vin': vin[i][0].replace(' ','')})
+                extracted_values.update({'vin': vin[i][0].replace(' ', '')})
         # print(vin.group())
 
+    # ICO
+    ICO = try_multiple_regex(unaccented_upper_text, reg_ico)
+    if ICO:
+        extracted_values.update({'ico': ICO})
 
     return extracted_values
 
@@ -76,29 +81,77 @@ def load_target_values(filename):
             return {}
 
 
-def calculate_accuracy(filename,extraction_method, extracted_values,elapsed_time):
-    print('extracted_values =>', extracted_values)
+def load_target_values_excel1():
+    ps = openpyxl.load_workbook('validation\\Faktury-template.xlsx')
+    sheet = ps['Sheet1']
+    print(sheet.max_row)
+    total_info = {}
+    for row in range(2, sheet.max_row + 1):
+        filename = sheet['A' + str(row)].value
+        nazov = sheet['B' + str(row)].value
+        ico = sheet['C' + str(row)].value
+        iban = sheet['D' + str(row)].value
+        cislo_fakt = sheet['E' + str(row)].value
+        datum_dodania = sheet['F' + str(row)].value
+        datum_vyhotovenia = sheet['G' + str(row)].value
+        cez_bez_dph = sheet['H' + str(row)].value
+        cena_s_dph = sheet['I' + str(row)].value
+        mena = sheet['J' + str(row)].value
+        ecv = sheet['K' + str(row)].value
+        vin = sheet['L' + str(row)].value
 
-    target_values = load_target_values(filename)
+        total_info.setdefault(filename, {'nazov': None,
+                                         'ico': '',
+                                         'iban': '',
+                                         'cislo_fakt': '',
+                                         'datum_dodania': '',
+                                         'datum_vyhotovenia': '',
+                                         'cez_bez_dph': '',
+                                         'cena_s_dph': '',
+                                         'mena': '',
+                                         'ecv': '',
+                                         'vin': ''
+                                         })
+
+        total_info[filename]['nazov'] = nazov
+        total_info[filename]['ico'] = ico
+        total_info[filename]['iban'] = iban
+        total_info[filename]['cislo_fakt'] = cislo_fakt
+        total_info[filename]['datum_dodania'] = datum_dodania
+        total_info[filename]['datum_vyhotovenia'] = datum_vyhotovenia
+        total_info[filename]['cez_bez_dph'] = cez_bez_dph
+        total_info[filename]['cena_s_dph'] = None if cena_s_dph is None else str(cena_s_dph).replace('.', ',')
+        total_info[filename]['mena'] = mena
+        total_info[filename]['ecv'] = ecv
+        total_info[filename]['vin'] = vin
+
+    return total_info
+
+
+def calculate_accuracy(filename,target_values, extraction_method, extracted_values, elapsed_time):
+    print('extracted_values =>', extracted_values)
     if not target_values:
         print('Nepodarilo sa nacitat cielove hodnoty pre porovnanie')
     else:
         print('target_values =>', target_values)
-        count = 0
+        count_hit = 0
+        count_all = 0
         for key in target_values.keys():
-            if target_values.get(key) == extracted_values.get(key):
-                count += 1
-        accuracy = count / len(target_values.keys()) * 100
+            if target_values.get(key) is not None:
+                count_all += 1
+                if target_values.get(key) == extracted_values.get(key):
+                    count_hit += 1
+        accuracy = count_hit / count_all * 100
         print('presnost = ', accuracy, ' %')
-    save_to_csv(filename,extraction_method,target_values,extracted_values,elapsed_time)
+    save_to_csv(filename, extraction_method, target_values, extracted_values, elapsed_time)
 
 
-def extract_qr_code(full_path):
+def extract_qr_code(full_path,extraction_method):
     extracted_values = {}
     extension = splitext(full_path)[1]
     qr = None
     if extension in ('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif'):
-        img = cv2.imread(full_path)
+        img = cv2.imdecode(np.fromfile(full_path, dtype=np.uint8), -1)
         qr = decode(img)
     elif extension == '.pdf':
         img = convert_from_path(full_path)
@@ -107,22 +160,45 @@ def extract_qr_code(full_path):
     else:
         print('Zadany format dokumentu nie je podporovany')
 
-    if qr and qr[0][1]=='QRCODE':  # ak naslo zober prvy
+    if qr and qr[0][1] == 'QRCODE':  # ak naslo zober prvy
         qrcode = qr[0].data.decode('utf-8')
-        ekasa_response = requests.post('https://ekasa.financnasprava.sk/mdu/api/v1/opd/receipt/find',
+        if re.match('^SPD\*\d\.\d\*', qrcode):   # QR kod typu SPD - Short Payment Descriptor
+            extraction_method = 'QR - SPD'
+            extract_spd(qrcode,extracted_values)
+        else:
+            ekasa_response = requests.post('https://ekasa.financnasprava.sk/mdu/api/v1/opd/receipt/find',
                                        json={"receiptId": qrcode})
-        if ekasa_response and ekasa_response.json()['receipt']:
-            extracted_values.update({'suma': str(ekasa_response.json()['receipt']['totalPrice']).replace('.', ',')})
+            if ekasa_response and ekasa_response.json()['receipt']:
+             extracted_values.update({'cena_s_dph': str(ekasa_response.json()['receipt']['totalPrice']).replace('.', ',')})
+             extraction_method = 'QR - Ekasa'
+            else:
+                print('PAY-By-Square')
+                extraction_method = 'QR - PayBySquare'
 
-    return extracted_values
+
+    return extracted_values,extraction_method
 
 
-def extract_dynamic_fields(image, phrases):
-    dynamic_fields = {}
-     ### !!! TODO tu treba loopovat bacha, iba prva strana
+# vytiahni potrebne fieldy z SPD formatu, ktory je v tvare SPD*1.0*ACC:CZ64030*AM:6897*CC:CZK*DT:2020031....
+def extract_spd(qrcode,extracted_values):
+    split = qrcode.split('*')
+    spd = {}
+    for s in split[2:-1]:
+        s_split = s.split(':')
+        spd.update({s_split[0]: s_split[1]})
+    extracted_values.update({'cena_s_dph':spd['AM']})
+
+
+# dynamicke vytahovanie hodnot podla regex
+def extract_dynamic_fields(image, phrases, extracted_values):
+    dynamic_fields = extracted_values
+    if (len(image.shape) >2):
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    else:
+        gray_image = image
     height, width = image.shape[:2]
     # converting image into gray scale image
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
     threshold_img = cv2.threshold(gray_image, 100, 255, cv2.THRESH_BINARY)[1]
     threshold_img1 = cv2.threshold(gray_image, 100, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
     dst = cv2.resize(threshold_img, (int(width / 2), int(height / 2)))
@@ -137,50 +213,65 @@ def extract_dynamic_fields(image, phrases):
     # print(extracted_dynamic_fields_temp)
     # print(extracted_dynamic_fields1_temp)
     # add as key:value pair
-    for i, (key,value) in enumerate(phrases.items()):
-        target_word = check_and_extract(key,value, extracted_dynamic_fields)
+    for i, (key, value) in enumerate(phrases.items()):
+        target_word = check_and_extract(key, value, extracted_dynamic_fields)
         if not target_word:
-            target_word = check_and_extract(key,value, extracted_dynamic_fields1)
+            target_word = check_and_extract(key, value, extracted_dynamic_fields1)
 
         if target_word is not None:
-            # dynamic_fields.update({phrase['name']: target_word})
             dynamic_fields.update({key: target_word})
 
     # return JSON like object with phrase name and extracted values (amounts in EUR, etc..)  -> sum_total:20,26
     return dynamic_fields
 
-def check_and_extract(key,phrase, extracted_dynamic_fields):
-    # extract the correct row and all words in a row by searching for the template phrase
-    correct_row = extract_correct_row(extracted_dynamic_fields, phrase)
-    # find final value in a row through regexp
-    if correct_row:
-        return find_final_value(str(correct_row),key)
+
+# extrahovanie textu cez Tesseract podla klucovych slov. Extrahovanie konkretneho riadku a vyparsovanie cielovej hodnoty
+def check_and_extract(key, phrase, extracted_dynamic_fields):
+    if not extracted_dynamic_fields['text'].isnull().values.all():
+        correct_row = extract_correct_row(extracted_dynamic_fields, phrase)
+        if correct_row:  # find final value in a row through regexp
+            return find_final_value(str(correct_row), key)
+    else:
+        return '-'
 
 
-# find the value based on defined type
+# hladanie konkretnej hodnoty podla zadefinovaneho typu. Vstupom je cely riadkok extrahovaneho textu, v ktorom by sa mala nachadzat hladana hodnota
 def find_final_value(row, template_type):
-    if template_type =='suma':
+    if template_type == 'cena_s_dph':
         amount = try_multiple_regex(row, reg_total_amount_number)
-        return amount.replace('.',',')
-    elif template_type =='iban':
+        if amount:
+            return amount.replace('.', ',')
+        else:
+            return None
+    elif template_type == 'iban':
         row1 = re.sub(r'[^a-zA-Z0-9]', '', row)
-        group = re.search(reg_iban, str(row1)).group()
-        if group.startswith('5'):
-            group='S'+group[1:]
-        return group
-    elif template_type =='vin':
-        return re.search(reg_vin, str(row)).group()
+        re_search = re.search(reg_iban, str(row1))
+        if re_search:
+            group = re_search.group()
+            if group.startswith('5'):
+                group = 'S' + group[1:]
+            return group
+    elif template_type == 'vin':
+        search = re.search(reg_vin, str(row))
+        if search:
+            return search.group()
+        else:
+            return None
     elif template_type == 'ecv':
-        return re.search(reg_ecv, str(row)).group()
+        search = re.search(reg_ecv, str(row))
+        if search:
+            return search.group()
+        else:
+            return None
 
 
+# hlada sa dana fraza ku ktorej sa priradi jej koordinat TOP.. nasledne sa vytiahnu vsetky slova v riadku podla predpokladu, ze ich TOP hodnota pripadne do intervalu [TOP-8, TOP+100]
 def extract_correct_row(extracted_dynamic_fields, phrase):
-    # split tepmlate phrase into words
-    phrase_split = phrase.split()
+    phrase_split = phrase.split()     # split search phrase into words
     text_split = []
     for i in phrase_split:
         text_split.append(i)
-        text_split.append(i+':')
+        text_split.append(i + ':')
     extracted_dynamic_fields['text'] = extracted_dynamic_fields['text'].str.upper()
     for word in text_split:
         top_values = extracted_dynamic_fields[extracted_dynamic_fields['text'] == word]['top'].values
@@ -215,31 +306,35 @@ def extract_correct_row(extracted_dynamic_fields, phrase):
             return correct_row
 
 
+# metoda na vyskusanie hladania pre pole regexpov.. pozor, poradie je dolezite, ak sa slovo najde loop konci, preto treba zadavat regexy od najvaic specifickej k najmenej specifickej
 def try_multiple_regex(row, regexp):
     final_extraction = None
     for i in regexp:
         final_extraction = re.search(i, str(row))
         if final_extraction:
-            final_extraction = final_extraction.group().replace(' ', '')
+            final_extraction = final_extraction.group(1).replace(' ', '')
             break
 
     return final_extraction
 
-def save_to_csv(filename,extraction_method,target_values, extracted_values,elapsed_time):
-    csv_columns= ['filename', 'duration','method','suma_target', 'suma_extracted','iban_target','iban_extracted','ecv_target', 'ecv_extracted','vin_target','vin_extracted' ]
+
+#ulozenie extahovanych aj cielovych hodnot do vysledneho suboru kvoli analyze presnosti extrahovania
+def save_to_csv(filename, extraction_method, target_values, extracted_values, elapsed_time):
+    csv_columns = ['filename', 'duration', 'method', 'ico_target', 'ico_extracted','cena_s_dph_target', 'cena_s_dph_extracted', 'iban_target', 'iban_extracted',
+                   'ecv_target', 'ecv_extracted', 'vin_target', 'vin_extracted']
     dict_data = [
-        {'filename': filename,'duration':elapsed_time, 'method':extraction_method,
-         'suma_target': target_values.get('suma',' -'), 'suma_extracted': extracted_values.get('suma',' -'),
-         'iban_target': target_values.get('iban',' -'), 'iban_extracted': extracted_values.get('iban',' -'),
-         'ecv_target': target_values.get('ecv',' -'), 'ecv_extracted': extracted_values.get('ecv',' -'),
-         'vin_target': target_values.get('vin',' -'), 'vin_extracted': extracted_values.get('vin',' -'),
+        {'filename': filename, 'duration': elapsed_time, 'method': extraction_method,
+         'ico_target': target_values.get('ico', ' -'), 'ico_extracted': extracted_values.get('ico', ' -'),
+         'cena_s_dph_target': target_values.get('cena_s_dph', ' -'), 'cena_s_dph_extracted': extracted_values.get('cena_s_dph', ' -'),
+         'iban_target': target_values.get('iban', ' -'), 'iban_extracted': extracted_values.get('iban', ' -'),
+         'ecv_target': target_values.get('ecv', ' -'), 'ecv_extracted': extracted_values.get('ecv', ' -'),
+         'vin_target': target_values.get('vin', ' -'), 'vin_extracted': extracted_values.get('vin', ' -'),
          }]
     csv_file = "data.csv"
     try:
-        with open(csv_file, 'a',newline='') as csvfile:
+        with open(csv_file, 'a', newline='', encoding="utf-8") as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
             for data in dict_data:
                 writer.writerow(data)
     except IOError:
         print("I/O error")
-
